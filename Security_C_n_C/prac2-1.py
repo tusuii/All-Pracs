d6fac53ef69887fdd93ff5700200d97c7a64348f
subodh kamble <39451041+tusuii@users.noreply.github.com>
2023-09-07 21:12:38 +0530

    Create prac2-1.py
    prac 2 part 1

---
+++ Security_C_n_C/prac2-1.py
@@ -0,0 +1,30 @@
+# practical 2.1 : Substitution Cipher 
+
+import string
+import random
+
+def generate_substitution_key():
+    alphabet = list(string.ascii_lowercase)
+    random.shuffle(alphabet)
+    return ''.join(alphabet)
+
+def cipher_encrypt(text, key):
+    alphabet = string.ascii_lowercase
+    table = str.maketrans(alphabet, key)
+    return text.translate(table)
+
+def cipher_decrypt(text, key):
+    alphabet = string.ascii_lowercase
+    table = str.maketrans(key, alphabet)
+    return text.translate(table)
+
+
+plaintext = str(input("Enter text"))
+
+substitution_key = generate_substitution_key()
+encrypted_text = cipher_encrypt(plaintext, substitution_key)
+decrypted_text = cipher_decrypt(encrypted_text, substitution_key)
+
+print("Key:", substitution_key)
+print("Cipher Encrypted:", encrypted_text)
+print("Cipher Decrypted:", decrypted_text)
