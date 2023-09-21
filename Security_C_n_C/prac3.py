8ff89b8774cee3c955a23626f03c2ac322e32029
subodh kamble <39451041+tusuii@users.noreply.github.com>
2023-09-21 23:27:18 +0530

    Create prac3.py

---
+++ Security_C_n_C/prac3.py
@@ -0,0 +1,33 @@
+# breaking Caesar
+def decrypt(ciphertext, shift):
+    de_txt = ""
+    
+    for char in ciphertext:
+        if char.isalpha():
+            is_upper = char.isupper()
+            char = char.lower()
+            de_char = chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))
+            if is_upper:
+                de_char = de_char.upper()
+            de_txt += de_char
+        else:
+            de_txt += char
+    
+    return de_txt
+
+
+def break_cipher(ciphertext):
+    for shift in range(26):
+        de_txt = decrypt(ciphertext, shift)
+        print(f"Shift {shift}: {de_txt}")
+
+if __name__ == "__main__":
+    ciphertext = input("Enter encrypted text: ")
+    break_cipher(ciphertext)
+
+# def decrypt(ciphertext, shift):
+#     return ''.join([
+#         chr(((ord(char.lower()) - ord('a') - shift) % 26) + ord('a')).upper() if char.isupper() else
+#         chr(((ord(char) - ord('a') - shift) % 26) + ord('a')) if char.isalpha() else char
+#         for char in ciphertext
+#     ])
