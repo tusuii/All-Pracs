ef832a45cbcffcf60fc13784011eb52fa69257bd
subodh kamble <39451041+tusuii@users.noreply.github.com>
2023-12-03 23:39:57 +0530

    Create Todo.sol

---
+++ Advance_computing(web_3)/Todo.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() {
+    createTask("TODO Goes Here!!!");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+}
