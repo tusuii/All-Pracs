bbb7dbc1819255878b174de13a6e0a4d61bd1330
subodh kamble <39451041+tusuii@users.noreply.github.com>
2023-12-26 21:55:44 +0530

    added prac4
    practical 4

---
+++ Data_science_visualisation/Practical4.R
@@ -0,0 +1,21 @@
+install.packages("tidyverse", type="source")
+library(tidyverse)
+
+head(billboard)
+
+#pivot_longer(): Pivot data from wide to long
+billboard_long <- billboard |> 
+  pivot_longer(
+    cols = starts_with('wk'),
+    names_to = 'week',
+    values_to = 'rank'
+  )
+
+billboard_long
+
+#pivot_wider(): Pivot data from long to wide
+billboard_long |> 
+  pivot_wider(
+    names_from = week,
+    values_from = rank
+  )
